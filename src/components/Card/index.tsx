eb281311d0eee90be3bf7888e955d76302aaeb12
twothreeMin <foreverything1023@gmail.com>
2022-07-13 20:10:29 +0900

    [Add: component] Card Component 제작

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { CustomerCard } from './style';
+import {
+  CardContainer, CardWrapper, CardLogo, CardCustomerName, CardTerm,
+} from './style';
 
 export const Card = () => (
-  <CustomerCard>
-    hi
-  </CustomerCard>
+  <CardContainer>
+    <CardWrapper>
+      <CardLogo src="https://cdn.econovill.com/news/photo/202012/510606_412941_2730.jpg" />
+      <CardCustomerName>FnGuide</CardCustomerName>
+      <CardTerm>2022.01.07 ~ 2022.02.14</CardTerm>
+    </CardWrapper>
+  </CardContainer>
 );
--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -1,5 +1,39 @@
+import styled from 'styled-components';
 import tw from 'tailwind-styled-components';
 
-export const CustomerCard = tw.button`
-  text-sky-400
+const CardContainer = tw.div`
+  max-w-sm
+  bg-white
+  rounded-lg
+  border
+  border-gray-200
+  shadow-md
 `;
+
+const CardWrapper = tw.div`
+  flex
+  flex-col
+  items-center
+  px-8
+  py-8
+`;
+
+const CardLogo = styled.img`
+  ${tw`p-2`}
+`;
+
+const CardCustomerName = tw.h5`
+  mb-1
+  text-xl
+  font-medium
+  text-gray-900
+`;
+
+const CardTerm = tw.span`
+  text-sm
+  text-gray-500
+`;
+
+export {
+  CardContainer, CardWrapper, CardLogo, CardCustomerName, CardTerm,
+};
