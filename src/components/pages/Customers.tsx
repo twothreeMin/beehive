a955dfb2f8960c2f29390ce807f300aa30e75ce4
twothreeMin <foreverything1023@gmail.com>
2022-10-18 23:21:07 +0900

    [add : customer compoent] Customer 데이터 추가

--- src/components/pages/Customers.tsx
+++ src/components/pages/Customers.tsx
@@ -1,7 +1,37 @@
+import axios, { AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react';
+import { Customer } from '../../interface/interface';
+import { Card } from '../Card/index';
+
 export function Customers() {
+  const [customers, setCustomers] = useState<Customer[]>([]);
+
+  useEffect(() => {
+    async function getCustomers() {
+      const response: AxiosResponse = await axios.get('https://beehive-dafc1-default-rtdb.asia-southeast1.firebasedatabase.app/customer.json');
+
+      const resultData: Customer[] = Object.keys(response.data).map((key: string) => ({
+        id: key,
+        cmpAddr: response.data[key].cmp_addr,
+        cmpName: response.data[key].cmp_name,
+        mgrName: response.data[key].mgr_name,
+        mgrPhone: response.data[key].mgr_phone,
+        mgrPosition: response.data[key].mgr_position,
+        project: response.data[key].project,
+      }));
+
+      setCustomers(resultData);
+    }
+    getCustomers();
+  }, []);
+
   return (
     <div>
-      <h2>고객사 페이지입니다.</h2>
+      {customers.map((customer) => (
+        <div key={customer.id}>
+          <Card props={customer} />
+        </div>
+      ))}
     </div>
   );
 }
--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -1,6 +1,22 @@
 export interface Consultant {
-  id : string,
-  name : string,
-  phone : string,
-  position: string
+  id : string;
+  name : string;
+  phone : string;
+  position: string;
+}
+
+export interface Customer {
+  // cmp: 회사, mgr: 담당자
+  id: string;
+  cmpAddr: string;
+  cmpName: string;
+  mgrName: string;
+  mgrPhone: string;
+  mgrPosition: string;
+  project: { // project: 프로젝트 정보
+    end_date: string;
+    name: string;
+    ptst_target: number;
+    start_date: string;
+  };
 }
