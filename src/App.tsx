6d23b30a3911011dd0e45b3ce387dd6452c2f7ff
twothreeMin <foreverything1023@gmail.com>
2022-10-26 22:25:09 +0900

    [add]react-query 감싸기

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Consultants } from './components/pages/Consultants';
 import { Customers } from './components/pages/Customers';
 import { Main } from './components/pages/Main';
 
@@ -7,6 +8,7 @@ export function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/consultants" element={<Consultants />} />
         <Route path="/customers" element={<Customers />} />
       </Routes>
     </BrowserRouter>
