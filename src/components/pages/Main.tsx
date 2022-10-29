90c5442bbe6a3d0fb7795966b422df0d878954e2
twothreeMin <foreverything1023@gmail.com>
2022-10-29 20:44:40 +0900

    [adding..] modal 창

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import { Modal } from '../Modal';
 
 export function Main() {
+  // const [modal, setModal] = useState(false);
+
   function openModal() {
     console.log('click!');
   }
 
   return (
     <div>
+      <Modal />
       <h1>Main</h1>
       <button type="button" onClick={openModal}>등록(모달창)</button>
-      <Modal />
     </div>
   );
 }
