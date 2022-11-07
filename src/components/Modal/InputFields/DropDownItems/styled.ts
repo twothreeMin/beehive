1eb23b3c6ea7dd42d3fc93ef0114f9eacb7eabb7
twothreeMin <foreverything1023@gmail.com>
2022-11-07 21:35:05 +0900

    [fix: css] 검색 dropdownitem 정렬

---
+++ src/components/Modal/InputFields/DropDownItems/styled.ts
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+import tw from 'twin.macro';
+
+export const DropDownItemsWrapper = styled.div`
+ ${tw`relative`}
+`;
+
+export const ItemsWrapper = styled.div`
+  ${tw`absolute bg-gray-50 w-full rounded-lg`}
+`;
+
+export const Item = styled.div`
+  & {
+    ${tw`py-2 px-3 flex space-x-44 place-content-center`}
+    ${tw`hover:bg-gray-100`}
+    ${tw`active:bg-gray-200`}
+  }
+
+  & #name {
+    ${tw`flex-1 p-1 rounded-full bg-yellow-300 text-sm text-gray-50 text-center`}
+  }
+
+  & #position {
+    ${tw`flex-1 p-1 text-sm text-gray-400 text-center`}
+  }
+`;
